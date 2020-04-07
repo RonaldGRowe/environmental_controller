88e7728aaf43c4f8aae5b2988091b709901ff69c
RonaldGRowe <ronaldgilesrowe@gmail.com>
2020-04-07 15:35:19 -0500

    Update relaystatus.py

--- relaystatus.py
+++ relaystatus.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python/
 
-import os
+#comment out unused modules
+#import os
 import sys
 import json
-import time
+#import time
 import RPi.GPIO as GPIO
 
+#check for connection to webpage
 try:
   data = json.loads(sys.argv[1])
 except:
     print "ERROR"
     sys.exit(1)
 
+#check status of relays to send status to webpage
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 pin = [18,20,23,24,19]
 length = len(pin)
 x = 0
 result = [0,1,2,3,4]
+#preset all to off and only changes to on when necessary
 relaystatus = ["OFF","OFF","OFF","OFF","OFF"]
 while x < length: 
 	GPIO.setup(pin[x],GPIO.OUT)
@@ -25,5 +29,5 @@ while x < length:
 	if result[x] == 0:
     		relaystatus[x] = "ON"
 	x += 1
-
+#sends relaystatus to webpage
 print json.dumps(relaystatus)
