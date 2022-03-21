b0372617e926b417df3e663dc177fed21c28b310
Ron Rowe <ronaldgilesrowe@gmail.com>
2022-03-21 14:36:07 -0500

    Continuing to optimize and update programs

--- environmentconditions.py
+++ environmentconditions.py
@@ -1,32 +1,36 @@
 #! /usr/bin/env python3
 
 from pigpio_dht import DHT22
-
+import time
 
 
 
 #using dht22 sensor to fill variables with current temperature and humidity
 def get_environment():
-    temperatures = []
-    humiditys = []
+    temperature = []
+    humidity = []
 
     tempsensorpins = [6]
 
     for pin in tempsensorpins:
-
-        sensor = DHT22(pin)
-        temperature.append(sensor['temp_f'])
-        humidity.append(sensor['humidity'])
+        tempf, humidity = sample_sensor(pin)
+        temperature.append(tempf)
+        humidity.append(humidity)
 
     return temperature, humidity
-x = 0
     
-    sensor = DHT22(6)
-    while x <= 4:
+
+def sample_sensor(pin):
+    temps = []
+    humiditys = []
+
+    sensor = DHT22(pin)
+    for c in range(3):
         result = sensor.read()
-        x += 1
-        time.sleep(5)
+        time.sleep(3)
         if result['valid']:
             temps.append(result['temp_f'])
             humiditys.append(result['humidity'])
     tempf = round(sum(temps)/len(temps),2)
+    humidity = round(sum(humiditys)/len(humiditys),2)
+    return tempf, humidity
--- storeconditionsindb.py
+++ storeconditionsindb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from environmentconditions import get_environment
 import mariadb
 from pigpio_dht import DHT22
 import time
@@ -13,26 +14,12 @@ conn = mariadb.connect(
 
 cur = conn.cursor()
 
-def sensortest():
-    x = 0
-    temps = []
-    humiditys = []
-    sensor = DHT22(6)
-    now = datetime.datetime.now()
-    while x <= 4:
-        result = sensor.read()
-        x += 1
-        time.sleep(5)
-        if result['valid']:
-            temps.append(result['temp_f'])
-            humiditys.append(result['humidity'])
-    tempf = round(sum(temps)/len(temps),2)
-#    print(tempf)
-#    print(temps)
-#    print(humiditys)
-    statement = "INSERT INTO readings (dtg, temperature, humidity) VALUES (?, ?, ?)"
-    cur.execute(statement, (now, tempf, humiditys[2],))
+def store_envrionment():
+    tempf, humidity = get_environment()
+    for i, temp in enumerate(tempf):
+        statement = "INSERT INTO readings (dtg, temperature, humidity) VALUES (?, ?, ?)"
+        cur.execute(statement, (now, temp, humidity[i],))
+    conn.commit()
+    conn.close()
 
-sensortest()
-conn.commit()
-conn.close()
+    store_environment()
