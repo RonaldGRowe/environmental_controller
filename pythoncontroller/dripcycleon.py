102e4bc1c9a379545153aed74d4a1df09700459f
Ron Rowe <ronaldgilesrowe@gmail.com>
2022-03-23 09:24:48 -0500

    create folder and move files

---
+++ pythoncontroller/dripcycleon.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import time
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+drippins = [19]
+
+nowhour = time.strftime('%H')
+nowmin = time.strftime('%M')
+
+#Use military time 24hrs
+driponhours = []
+
+#Use seconds only, no minutes or hours
+driponsecs = 60
+
+if nowhour in driponhours:
+    for pin in drippins:
+        GPIO.setup(pin,GPIO.OUT)
+        status = GPIO.input(pin)
+        if status:
+                    GPIO.output(pin,GPIO.LOW)#turn drip on
+    sleep(driponsecs)
+    GPIO.output(pin,GPIO.HIGH)#turn drip off
+
