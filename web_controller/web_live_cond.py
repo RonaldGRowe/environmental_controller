c692b4d6207614e3751fd355ac76f0d8d09f99d4
ronaldgrowe <ronaldgrowe@hotmail.com>
2022-04-11 16:43:48 -0500

    Fixing bugs

--- web_controller/web_live_cond.py
+++ web_controller/web_live_cond.py
@@ -7,11 +7,11 @@ import json
 #from get_water_temp import readwatertemp
 
 #used to test web connection to script
-try:
-    data = json.loads(sys.argv[1])
-except:
-    print("ERROR")
-    sys.exit(1)
+#try:
+#    data = json.loads(sys.argv[1])
+#except:
+#    print("ERROR")
+#    sys.exit(1)
 
 
 #read temperatures of the thermocouple
@@ -20,6 +20,5 @@ except:
 
 readings={"watertemp": 66, "secwatertemp": 63, "airtemp": 78, "secairtemp": 83, "humidity": 78, "sechumidity": 67}
 
-
 #send set of readings to webpage
-json.dumps(reading)
+print(json.dumps(readings))
