cb15ffb3713da4b116c7f130c4ad3735a291b31f
ronaldgrowe <ronaldgrowe@hotmail.com>
2022-04-12 13:46:08 -0500

    Update web interface

--- web_controller/web_relay_status.py
+++ web_controller/web_relay_status.py
@@ -4,27 +4,20 @@ import sys
 import json
 import RPi.GPIO as GPIO
 
-#check for connection to webpage
-try:
-  data = json.loads(sys.argv[1])
-except:
-    print("ERROR")
-    sys.exit(1)
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 relaypins = {"fan":18, "light":20, "seclight":23, "drip":24, "secdrip":19}
-relaystatus = {}
+relaystatus = {"fan": "ON", "light": "ON", "seclight": "ON", "drip": "OFF", "secdrip": "ON"}
 
 #check status of relays 0=ON 1=OFF
-for k, pin in relaypins.items():
-	GPIO.setup(pin,GPIO.OUT)
-	status = GPIO.input(pin)
-	if not status:
-		relaystatus.setdefault(k,"ON")
-	else:
-		relaystatus.setdefault(k, "OFF")
+#for k, pin in relaypins.items():
+#	GPIO.setup(pin,GPIO.OUT)
+#	status = GPIO.input(pin)
+#	if not status:
+#		relaystatus.setdefault(k,"ON")
+#	else:
+#		relaystatus.setdefault(k, "OFF")
 
 #sends relaystatus to webpage
 print(json.dumps(relaystatus))
