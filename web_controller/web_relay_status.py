080c75272d024101a3e5d441dd7df6df21e4ed69
ronaldgrowe <ronaldgrowe@hotmail.com>
2022-04-12 14:32:12 -0500

    Fix bug

--- web_controller/web_relay_status.py
+++ web_controller/web_relay_status.py
@@ -8,16 +8,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 relaypins = {"fan":18, "light":20, "seclight":23, "drip":24, "secdrip":19}
-relaystatus = {"fan": "ON", "light": "ON", "seclight": "ON", "drip": "OFF", "secdrip": "ON"}
+relaystatus = {}
 
-#check status of relays 0=ON 1=OFF
-#for k, pin in relaypins.items():
-#	GPIO.setup(pin,GPIO.OUT)
-#	status = GPIO.input(pin)
-#	if not status:
-#		relaystatus.setdefault(k,"ON")
-#	else:
-#		relaystatus.setdefault(k, "OFF")
+#Check status of relays 0=ON 1=OFF
+for k, pin in relaypins.items():
+	GPIO.setup(pin,GPIO.OUT)
+	status = GPIO.input(pin)
+	if not status:
+		relaystatus.setdefault(k,"ON")
+	else:
+		relaystatus.setdefault(k, "OFF")
 
-#sends relaystatus to webpage
+#Sends relaystatus to webpage
 print(json.dumps(relaystatus))
