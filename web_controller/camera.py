1c2ee7bb7f8e0d3abdfe932741f6c0b1871d6d62
Ronald Rowe <ronaldgilesrowe@gmail.com>
2022-05-02 09:42:16 -0500

    change jpg name

--- web_controller/camera.py
+++ web_controller/camera.py
@@ -5,7 +5,7 @@ from picamera import PiCamera
 
 def still_capture_jpg():
     camera = PiCamera()
-    camera.capture('/var/www/html/pic3.jpg')
+    camera.capture('/var/www/html/pic1.jpg')
 
 
 if __name__ == "__main__":
